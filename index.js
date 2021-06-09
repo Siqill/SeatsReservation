9123bc0119e552409fff487a4abb0e8b5eae7d14
yahor <egorshavluk98@gmail.com>
2021-06-10 00:23:53 +0200

    v 1.02

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 "use strict";
 
-submit.onclick = function() {
-    let seats = document.getElementById('seats');
-    let isNeibhor = document.getElementById('neighbor');
-    localStorage.setItem('numOfSeats', seats.value);
-    localStorage.setItem('isNeibhor', isNeibhor.checked);
+let div;
+
+submit.onclick = function (e) {
+    // prevents submit if seats < 1 and focus on input
+    if (!seats.value) {
+        e.preventDefault();
+        seats.focus();
+    }
+    else {
+        localStorage.setItem('numOfSeats', seats.value);
+        localStorage.setItem('isNeibhor', neighbor.checked);
+    }
 }
 
+//shows a hint if there are fewer than 2 seats and prevents highlight
+neighbor.onclick = function (e) {
+    if (seats.value < 2 && e.target.checked) {
+        e.target.checked = false;
+        let coords = e.target.getBoundingClientRect();
+        div = document.createElement('div');
+        div.className = 'tip';
+        div.innerHTML = 'Wybierz więcej <br> niż 1 miejsce';
+        document.body.append(div);
+        div.style.top = coords.top - div.offsetHeight / 2 + e.target.offsetHeight / 2 + "px";
+        div.style.left = coords.left - div.offsetWidth - 10 + 'px';
+    }
+}
+
+document.body.onclick = function (e) {
+    if (e.target.id != 'neighbor' && div) div.remove();
+}
 
 
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let message;
+
 let request;
 let seats = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,67 +13,62 @@ let seats = [
 ];
 
 
-
 {
-
     draw();
-    console.log(localStorage.getItem('numOfSeats'))
-    
 
+    //prevents selection
+    document.querySelector('.container').onmousedown = function () { return false; };
+
+    field.onmouseover = function (e) {
+        if (!e.target.classList.contains('border')) return;
+        let message;
+        message = document.createElement('div');
+        message.className = 'message';
+        message.innerText = `rzad:${e.target.dataset.y}, meijsce:${e.target.dataset.x}`;
+        document.body.append(message);
+        let coords = e.target.getBoundingClientRect();
+        message.style.top = coords.bottom + message.offsetHeight / 2 + "px";
+        message.style.left = coords.left + e.target.offsetWidth / 2 - message.offsetWidth / 2 + 'px';
+
+        field.onmouseout = function () {
+            if (message) message.remove();
+        };
+    };
+
+    field.onclick = function (e) {
+        if (!e.target.classList.contains('border')) return;
+        if (e.target.classList.contains('reserved')) return;
+
+        // does not allow you to select more seats than you marked early
+        if (field.querySelectorAll('.selection').length == localStorage.getItem('numOfSeats')) {
+            e.target.classList.remove('selection');
+            return;
+        }
+
+        e.target.classList.toggle('selection');
+        if (localStorage.getItem('isNeibhor') == 'true') getNeighbor(e.target);
+    };
+
+
+    reserve.onclick = function () {
+        let seats = field.querySelectorAll('.selection');
+        if (!seats.length) return;
+        let div = document.createElement('div');
+        div.className = 'container';
+        div.innerHTML = '<h2>Twoja rezerwacja przebiegła pomyślnie!</h2><br><br>Wybrałeś miejsca:<br>';
+        for (let seat of seats) {
+            div.innerHTML += `- rząd ${seat.dataset.y}, miejsce ${seat.dataset.x} (${seat.id})<br>`;
+            seat.classList.add('reserved');
+            seat.classList.remove('selection');
+        }
+        div.innerHTML += '<br><br><h3>Dziękujemy! W razie problemów prosimy o kontakt z działem administracji.</h3>';
+        document.body.innerHTML = "";
+        document.body.append(div);
+
+    };
 }
 
-document.querySelector('.container').onmousedown = function() {return false;};
-
-field.onmouseover = function(e) {
-    if (!e.target.classList.contains('border')) return;
-    message = document.createElement('div');
-    message.className = 'message';
-    message.innerText = `rzad:${e.target.dataset.y}, meijsce:${e.target.dataset.x}`;
-    document.body.append(message);
-    let coords = e.target.getBoundingClientRect();
-    message.style.top = coords.bottom + "px";
-    message.style.left = coords.left + e.target.offsetWidth / 2 - message.offsetWidth / 2 +'px';
-};
-field.onmouseout = function() {
-    if (message) message.remove();
-};
-
-
-reserve.onclick = function() {
-    let seats = field.querySelectorAll('.selection');
-    if (!seats.length) return;
-    let div = document.createElement('div');
-    div.className = 'container';
-    div.innerHTML = '<h2>Twoja rezerwacja przebiegła pomyślnie!</h2><br><br>Wybrałeś miejsca:<br>';
-    for (let seat of seats) {
-        div.innerHTML += `- rząd ${seat.dataset.y}, miejsce ${seat.dataset.x} (${seat.id})<br>`;
-        seat.classList.add('reserved');
-        seat.classList.remove('selection');
-    }
-    div.innerHTML += '<br><br><h3>Dziękujemy! W razie problemów prosimy o kontakt z działem administracji.</h3>';
-    document.body.innerHTML = "";
-    document.body.append(div);
-    
-};
-
-
-field.onclick = function(e) {
-    if (!e.target.classList.contains('border')) return;
-    if (e.target.classList.contains('reserved')) return;
-    
-    // nie pozwala wybrac wiecej mejsc niz zaznaczylem wczesnej
-    if (field.querySelectorAll('.selection').length >= localStorage.getItem('numOfSeats')) {
-        e.target.classList.remove('selection');
-        return;
-    }
-
-    e.target.classList.toggle('selection');
-};
-
-
-
-
-
+// Błąd w db.json dlatego tak maluje miejsca
 function draw() {
     let out = '';
     for (let i = 0; i < seats.length; i++) {
@@ -84,7 +79,6 @@ function draw() {
         out += '<br>';
     }
     document.querySelector('#field').innerHTML = out;
-
 
     getRequest();
 
@@ -100,7 +94,7 @@ function draw() {
                     if (seat.reserved)
                         node.classList.add('reserved');
                 }
-                
+
             }
         }
     }
@@ -121,5 +115,17 @@ function getRequest() {
     request.send();
 }
 
+function getNeighbor(elem) {
+    let x = +elem.dataset.x;
+    let y = +elem.dataset.y;
+    let next = field.querySelector(`.border[data-x="${x + 1}"][data-y="${y}"]`);
+    let previous = field.querySelector(`.border[data-x="${x - 1}"][data-y="${y}"]`);
+    if (next && !next.classList.contains('reserved')) {
+        next.classList.toggle('proposition');
+    }
+    if (previous && !previous.classList.contains('reserved')) {
+        previous.classList.toggle('proposition');
+    }
 
+}
 
