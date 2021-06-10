6013c7bb376ea78d099b07c3bdba4633b20d930b
yahor <egorshavluk98@gmail.com>
2021-06-10 20:05:42 +0200

    bugs fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let div;
 
 submit.onclick = function (e) {
     // prevents submit if seats < 1 and focus on input
-    if (!seats.value) {
+    if (!seats.value || seats.value < 1 || Object.is(+seats.value, NaN)) {
         e.preventDefault();
         seats.focus();
     }
@@ -14,22 +14,40 @@ submit.onclick = function (e) {
     }
 }
 
-//shows a hint if there are fewer than 2 seats and prevents highlight
-neighbor.onclick = function (e) {
-    if (seats.value < 2 && e.target.checked) {
-        e.target.checked = false;
-        let coords = e.target.getBoundingClientRect();
-        div = document.createElement('div');
-        div.className = 'tip';
-        div.innerHTML = 'Wybierz więcej <br> niż 1 miejsce';
-        document.body.append(div);
-        div.style.top = coords.top - div.offsetHeight / 2 + e.target.offsetHeight / 2 + "px";
-        div.style.left = coords.left - div.offsetWidth - 10 + 'px';
-    }
-}
 
 document.body.onclick = function (e) {
-    if (e.target.id != 'neighbor' && div) div.remove();
+    //shows a hint if there are fewer than 2 seats and prevents highlights
+    if (e.target.id == 'neighbor') {
+        if (seats.value < 2 && e.target.checked) {
+            e.target.checked = false;
+            if (div) return;
+            let coords = e.target.getBoundingClientRect();
+            div = document.createElement('div');
+            div.className = 'tip';
+            div.innerHTML = 'Jeśli chcesz wybrać tę opcję<br>zaznacz więcej niż 1 miejsce';
+            document.body.append(div);
+            div.style.top = coords.top - div.offsetHeight / 2 + e.target.offsetHeight / 2 + "px";
+            div.style.left = coords.left - div.offsetWidth - 10 + 'px';
+        }
+    }
+    if (Object.is(+seats.value, NaN)) neighbor.checked = false; 
 }
 
+
+seats.onfocus = function(e) {
+    if (div) {
+        div.remove();
+        div = null;
+    }
+    
+    e.target.classList.remove('danger');
+
+    e.target.onblur = function() {
+        if (e.target.value < 2 && neighbor.checked) neighbor.checked = false;
+
+        if (e.target.value < 1 || Object.is(+e.target.value, NaN)) {
+            e.target.classList.add('danger');
+        }
+    }
+}
 
